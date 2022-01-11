24a30aa748402764e0e2f5af3203e1d1603d1eac
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-11 07:37:50 +0200

    importing npm modules

--- week1/example1/app.js
+++ week1/example1/app.js
@@ -1,9 +1,21 @@
+const validator=require('validator');
 const getNotes= require('./notes');
 
-// const [firstName,add]= require('./utils.js');
 
 console.log(getNotes());
+console.log(validator.isEmail('sivan@example.com'));
+console.log(validator.isEmail('@example.com'));
+///
+console.log(validator.isURL('https://example.com'));
+console.log(validator.isURL('https:/example.com'));
 
+
+
+
+
+//using my own files reference: 
+
+// const [firstName,add]= require('./utils.js');
 // console.log(firstName);
 // console.log(add(2,3));
 
