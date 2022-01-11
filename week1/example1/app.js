75d7e31563cd9cb55375c5495a289be8791a5e07
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-11 07:18:34 +0200

    importing my own files

---
+++ week1/example1/app.js
@@ -0,0 +1,9 @@
+const getNotes= require('./notes');
+
+// const [firstName,add]= require('./utils.js');
+
+console.log(getNotes());
+
+// console.log(firstName);
+// console.log(add(2,3));
+
---
+++ week1/example1/utils.js
@@ -0,0 +1,8 @@
+console.log("Utils.js");
+
+const name = 'Sivan';
+
+const add = function(a,b){
+    return a+b;
+}
+module.exports = [name, add];
