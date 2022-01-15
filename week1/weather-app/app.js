83f3c2eaa653d2ff9b0934e842cf45db6ee86e2a
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-15 08:51:16 +0200

    sync vs async

---
+++ week1/weather-app/app.js
@@ -0,0 +1,12 @@
+console.log("STARTING");
+
+setTimeout(()=>{
+    console.log(" 2 seconds timer");
+},2000)
+setTimeout(()=>{
+    console.log(" 0 seconds timer");
+},2000)
+
+
+
+console.log("STOPPING");
