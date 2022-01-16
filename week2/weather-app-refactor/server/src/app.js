5c7305de5c60aac80e16c0f37ce3c749ae182ad0
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-16 14:35:44 +0200

    weather-app-refactor-connecting express with react

--- week2/weather-app-refactor/server/src/app.js
+++ week2/weather-app-refactor/server/src/app.js
@@ -3,21 +3,10 @@ const express = require('express')
 // const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const cors = require('cors')
 
 const app = express()
-
-// Define paths for Express config
-// const publicDirectoryPath = path.join(__dirname, '../public')
-// const viewsPath = path.join(__dirname, '../templates/views')
-// const partialsPath = path.join(__dirname, '../templates/partials')
-
-// Setup handlebars engine and views location
-// app.set('view engine', 'hbs')
-// app.set('views', viewsPath)
-// hbs.registerPartials(partialsPath)
-
-// Setup static directory to serve
-// app.use(express.static(publicDirectoryPath))
+app.use(cors())
 
 
 app.get('/weather', (req, res) => {
