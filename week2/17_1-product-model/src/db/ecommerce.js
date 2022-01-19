f2b311e5eebe211f4a15489f3ee664920b377fb3
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-19 10:38:44 +0200

    ex17.1 products

--- week2/17_1-product-model/src/db/ecommerce.js
+++ week2/17_1-product-model/src/db/ecommerce.js
@@ -1,5 +1,6 @@
 const { log } = require('console');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/products-db', {
     useNewUrlParser: true,
@@ -18,56 +19,89 @@ const Product = mongoose.model('Product', {
         required: true
     },
     isActive: {
-        type: Boolean,
-        details: []
+        type: Boolean
+    },
+    details: {
+        desciption: {
+            type: String,
+            validate(description) {
+                if (description.length < 10) {
+                    throw new Error("description must have at least 10 characters")
+                }
+            },
+            required: true,
+            min: 10
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: -1
+        },
+        discount: {
+            type: Number,
+            default: 0
+        },
+        images: {
+            type: [String],
+            minLength: 2,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            validate: (/^\d{10}$/),
+            required: true
+        },
+    }}
+)
+
+
+
+
+//A Mongoose schema defines the structure of the document, default values, validators, etc., 
+//whereas a Mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
+
+//model is the decleration of the DB
+//schema is structure of the data inside the model
+
+
+const product = new Product({
+    name: "iphone",
+    category: "electronics",
+    isActive: true,
+    details: {
+        desciption: "buy now dont miss best prices!!",
+        price: 10,
+        images: ['img1', 'img2'],
+        phoneNumber: "0504080778"
+    }
+})
+const product2 = new Product({
+    name: "chair",
+    category: "furniture",
+    isActive: true,
+    details: {
+        desciption: "buy now dont miss best prices!!",
+        price: 10,
+        images: ['img1', 'img2'],
+        phoneNumber: "0503555666"
     }
 })
 
-const Details = mongoose.model('Details', {
-    desciption: {
-        type: String,
-        validate(description) {
-            if (description.length < 10) {
-                throw new Error("description must have at least 10 characters")
-            }
-        },
-        required: true,
-        // min: (10),
-    },
-    price: {
-        type: Number,
-        required: true,
-        // min: (-1)
-    },
-    discount: {
-        type: Number,
-        required: false,
-        default: 0
-    },
-    images: {
-        type: Array,
-        minlength: 2
-    },
-    // phoneNumber: {
-    //     type: ,
-
-
-    // }
-})
-
-
-const details = new Details({
-    desciption:"iphone",
-    price: 500,
+const product3 = new Product({
+    name: "table",
+    category: "furniture",
     isActive: true,
-    images:['one', 'two']
-})
-const product = new Product({
-    name:"iphone",
-    category:"electronics",
-    isActive: true,
-    details: (details)
+    details: {
+        desciption: "buy now dont miss best prices!!",
+        price: 10,
+        images: ['img1', 'img2'],
+        phoneNumber: "0506798554"
+    }
 })
 
-product.save().then(() => {console.log(product)}).catch((err) => {console.log(err)})
-details.save().then(() => {console.log(details)}).catch((err) => {console.log(err)})
+
+
+product.save().then(() => { console.log(product) }).catch((err) => { console.log(err) })
+product2.save().then(() => { console.log(product2) }).catch((err) => { console.log(err) })
+product3.save().then(() => { console.log(product3) }).catch((err) => { console.log(err) })
+// details.save().then(() => { console.log(details) }).catch((err) => { console.log(err) }) 
